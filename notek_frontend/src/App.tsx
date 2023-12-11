b082863ed2814ae90c9f383fec4ebd9f986f184d
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-12-11 12:54:33 +0300

    tauri

--- notek_frontend/src/App.tsx
+++ notek_frontend/src/App.tsx
@@ -1,11 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { MainPage, Editor } from './Pages'
+import { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { MainPage, Editor } from './Pages';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { EditorById } from './Pages/Editor/EditorById';
 import { Authorization } from './Pages/Authorization/Authorization';
 import { Registration } from './Widgets/Authorization/Registration';
 
+const { invoke } = (window as any).__TAURI__.tauri;
+
 function App() {
+  useEffect(() => {
+    // Create a new Tauri window
+    invoke('tauri', { cmd: 'create' })
+      .then((response: any) => console.log(response))
+      .catch((error: any) => console.error(error));
+
+    return () => {
+      // Optionally, close the Tauri window when the component unmounts
+      invoke('tauri', { cmd: 'close' })
+        .then((response: any) => console.log(response))
+        .catch((error: any) => console.error(error));
+    };
+  }, []);
 
   return (
     <>
@@ -19,7 +35,7 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
